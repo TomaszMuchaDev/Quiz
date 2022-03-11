0312c3e39831a985e9193aaffa4641c814fd26af
Tomasz <tomwasp5@gmail.com>
2022-03-11 15:05:58 +0000

    layout added.

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ const btnLogin = document.getElementById("btnLogin");
 const btnCreate = document.getElementById("btnCreate");
 const btnHome = document.getElementById("btnHome");
 const pointsMessage = document.getElementById("pointsMessage");
-const nameInput = document.getElementById("nameInput")
+const nameInput = document.getElementById("nameInput");
+const btnHowToPlayPage = document.getElementById("btnHowToPlay");
 
 let name;
 
-nameInput.addEventListener('input', (e) => {
-    name = e.target.value;
-});
+
 
 // use only on index page
     if (btnLogin) {
         pointsMessage.innerText = "Welcome";
         btnLogin.addEventListener("click", handleLogin)
-
+        nameInput.addEventListener('input', (e) => {
+            name = e.target.value;
+        });
 
     }
 // use with about and how to play pages
@@ -28,24 +29,27 @@ nameInput.addEventListener('input', (e) => {
         pointsMessage.innerText = `${player}`;
     }
 
-    function handleLogin() {
+if(btnHowToPlayPage){
+    btnHowToPlayPage.addEventListener("click", handleHowToPlayRedirection);
+}
+
+function handleLogin() {
        const userFromLocalStorage = localStorage.getItem(name);
        if(userFromLocalStorage)
        {
            let user = localStorage.getItem(name);
-
        }
        else
        {
            let player={name: name, score:0, topScore:0}
            localStorage.setItem("name", JSON.stringify(player));
        }
-
-
     }
 
-
-    function handleHome() {
+function handleHome() {
         window.location.href = "./index.html";
     }
 
+function handleHowToPlayRedirection (){
+    window.location.href = "./index.html";
+}
