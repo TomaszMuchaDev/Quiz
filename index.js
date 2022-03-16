2e209efaacd63128ae013d1769f0510abd1002e5
Tomasz <tomwasp5@gmail.com>
2022-03-16 16:25:04 +0000

    Map api added.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const resultFlag = document.getElementById("resultFlag");
 const resultFact = document.getElementById("resultFact");
 const resultBtnWiki = document.getElementById("resultBtnWiki");
 const resultMsg = document.getElementById("resultMsg");
-
+const regionMessage = document.getElementById("regionMessage");
 
 
 let name;
@@ -86,17 +86,38 @@ function handleResult (countryNumber){
             const name = json[countryNumber].name;
             const interestingFact = json[countryNumber].interestingFact;
             const wikiLink = json[countryNumber].wikiLink;
+            const lng = json[countryNumber].longitude;
+            const lat = json[countryNumber].latitude;
             resultFlag.src = json[countryNumber].flagPath;
             resultFact.innerText = "Do you know that " + interestingFact;
             resultBtnWiki.addEventListener("click", ()=>{
                 window.open(wikiLink);
             });
+            regionMessage.innerText = "Region is " + json[countryNumber].region;
             resultBtnWiki.innerText = name + " info";
             resultMsg.innerText = `This is flag of ${name}.
             Click button below to find out more about this country.`;
+            handleMapDisplay(lng, lat);
         });
 }
 
-handleResult(1);
+handleResult(9);
+
+
+function handleMapDisplay(lng, lat){
+    mapboxgl.accessToken = 'pk.eyJ1IjoidG9tYXN6bXVjaGEiLCJhIjoiY2wwdG1zMDN6MDE4aDNjbzg3cnZqNzhwcSJ9.D5dv2MRT52aVyObLS_gAiw';
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: [lng, lat], // starting position [lng, lat]
+        zoom: 5 // starting zoom
+    });
+
+    const nav = new mapboxgl.NavigationControl();
+    map.addControl(nav)
+    new mapboxgl.Marker({ color: 'red'})
+        .setLngLat([lng, lat])
+        .addTo(map);
+}
 
 
