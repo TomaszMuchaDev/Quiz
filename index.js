b06cea67d2c4962395e0cdd6c6ef8a5972e9aa29
Tomasz <tomwasp5@gmail.com>
2022-03-21 18:28:32 +0000

    chooseRegion.html updated.

--- index.js
+++ index.js
@@ -7,37 +7,110 @@ const resultFact = document.getElementById("resultFact");
 const resultBtnWiki = document.getElementById("resultBtnWiki");
 const resultMsg = document.getElementById("resultMsg");
 const regionMessage = document.getElementById("regionMessage");
+const regionCardBtn = document.querySelectorAll(".regionCard");
+const btnSubmitAnswer =document.getElementById("btnSubmitAnswer");
+const btnSubmitRegion = document.getElementById("btnSubmitRegion");
 
 
-let name;
-
-
-// use only on index page
-    if (btnLogin) {
+// index page
+if (btnLogin) {
         pointsMessage.innerText = "Welcome";
-        btnLogin.addEventListener("click", handleLogin)
         nameInput.addEventListener('input', (e) => {
-            name = e.target.value;
+        btnLogin.playerName  = e.target.value;
         });
-    }
-// rest of  pages
-    else {
-        // const player = localStorage.getItem("player");
-        // pointsMessage.innerText = `${player}`;
-    }
+        btnLogin.addEventListener("click", handleLogin);
+}
 
-function handleLogin() {
-       const userFromLocalStorage = localStorage.getItem(name);
+
+// choose region Page
+if(btnSubmitRegion) {
+    handleChooseRegion();
+}
+
+
+// game page
+if(btnSubmitAnswer){
+    handleGame();
+}
+
+// result page
+if(resultFlag){
+    handleResult(45);
+}
+
+
+
+function handleLogin(event) {
+    const name = event.currentTarget.playerName;
+    const userFromLocalStorage = localStorage.getItem(name);
        if(userFromLocalStorage)
        {
-           let user = localStorage.getItem(name);
+           let player = localStorage.getItem(name);
+
        }
        else
        {
            let player={name: name, score:0, topScore:0}
-           localStorage.setItem("name", JSON.stringify(player));
+           localStorage.setItem(name, JSON.stringify(player));
        }
     }
+
+
+function handleChooseRegion(){
+    let region;
+    regionCardBtn.forEach(btn =>{
+        btn.addEventListener("click", (e) => {
+            regionCardBtn.forEach(b => b.classList.remove("regionCardClicked"));
+            e.target.classList.add("regionCardClicked");
+            region = e.target.dataset.region;
+        });
+    });
+    btnSubmitRegion.addEventListener("click", ()=>{
+        localStorage.setItem("region", region);
+        window.location="./game.html";
+    });
+}
+
+
+
+function  handleGame(){
+    let allCountries;
+    const region = localStorage.getItem("region");
+    regionMessage.innerText="  Region is " + region + ".";
+    fetch("./countries.json")
+        .then(response => response.json())
+        .then(json =>displayGame(getRandomGameCountesses(json, region)));
+
+}
+
+// randomly pick 30 countries for given region
+// for all game round;
+function getRandomGameCountesses(countries, region){
+    let countriesFromRegion=[];
+
+    if(!region)
+    {
+        alert("Sorry we have some problem with reading data please try later.")
+    }
+   else if(region === "World")
+    {
+        //if region is world
+        countriesFromRegion = [...countries];
+    }
+    else{
+        //if region is other than world
+        countries.map(c=>{
+            c.region === region ? countriesFromRegion.push(c) : null;
+        });
+    }
+    return countriesFromRegion;
+}
+
+function displayGame(drawnCountries){
+
+    localStorage.setItem("drawnCountries",JSON.stringify(drawnCountries));
+}
+
 
 function handleResult (countryNumber){
     fetch("./countries.json")
@@ -53,16 +126,14 @@ function handleResult (countryNumber){
             resultBtnWiki.addEventListener("click", ()=>{
                 window.open(wikiLink);
             });
-            regionMessage.innerText = "Region is " + json[countryNumber].region;
+            const regionForMessage = json[countryNumber].region === "Asia" ? "Asia and Australia" : json[countryNumber].region;
+            regionMessage.innerText = "Region is " + regionForMessage + ".";
             resultBtnWiki.innerText = name + " info";
             resultMsg.innerText = `This is flag of ${name}.
             Click button below to find out more about this country.`;
             handleMapDisplay(lng, lat);
         });
 }
-
-
-handleResult(54);
 
 
 function handleMapDisplay(lng, lat){
